1278e29c7581f0f9e6bf8e8ff2da7e54bb8062c1
hieunguyen0802 <hieu.h.t.nguyen@tcu.edu>
2023-06-08 07:23:54 +0700

    working on form fe

--- client/src/Components/Test.jsx
+++ client/src/Components/Test.jsx
@@ -15,30 +15,32 @@ import Card from "react-bootstrap/Card";
 import Modal from "react-bootstrap/Modal";
 
 export default function TextFields() {
-  const [checkedFirstQuestion, setCheckedFirstQuestion] = useState(false);
-  const [checkedSecondQuestion, setCheckedSecondQuestion] = useState(false);
-  const refFirstQuestion = useRef(null);
-  const refSecondQuestion = useRef(null);
-  const [readyToConfirm, setReadyToConfirm] = useState(false);
+  const refFirstQuestion = useRef();
+  const refSecondQuestion = useRef();
   const [studentTable, setStudentTable] = useState();
-  const dataTable = useRef(null);
-  const [isGettingOTP, setGettingOTP] = useState(false);
-  const [isLogin, setIsLogin] = useState(false);
-  const [otp, setOtp] = useState("");
-  const refLogin = useRef("");
-  const [showAlert, setShowAlert] = useState("");
-  const [validateLogin, setValidateLogin] = useState(true);
-  const [isConfirm, setIsConfirm] = useState("");
-  const [confirmTime, setConfirmTime] = useState("");
-  const [firstHealthQuestion, setFirstHealthQuestion] = useState("");
-  const [secondHealthQuestion, setSecondHealthQuestion] = useState("");
   const [show, setShow] = useState(false);
-
+  const [dataModal, setDataModal] = useState()
+  const [ans,setAns] = useState()
+  const stuCode = dataModal != null ? dataModal.studentCode : dataModal
+  const firstDomId = "firstQuestion" + stuCode
+  const secondDomId = "secondQuestion" + stuCode
   const handleClose = () => setShow(false);
   const handleShow = (row) => {
-    console.log(row);
+    setDataModal(row);
     setShow(true);
   }
+  
+  const handleSave = (data) => {
+    console.log(refFirstQuestion.current.value)
+    console.log(refSecondQuestion.current.value)
+    console.log(data)
+    setShow(false)
+   
+  }
+  const ExpandedComponent = ({ data }) => <input ref={refFirstQuestion}/>
+
+  
+
   const handleClick = async () => {
     const params = "thuyduong032201@gmail.com";
     await fetch("/getTestTable?email=" + params, { method: "GET" })
@@ -96,91 +98,101 @@ export default function TextFields() {
           <Modal.Title>
             Thông Tin Về Sức Khỏe Để Tham Gia Chương Trình Trại Hè VAS 2023 /{" "}
             <i style={{ color: "blue" }}>
-              Health Information for VAS Summer Camp 2023 Participation
+              Health Information for VAS Summer Camp 2023 Participation {dataModal != null ? dataModal.studentCode : dataModal}
             </i>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="m-3">
-            <Form.Group
-              className="mb-2"
-              controlId="firstQuestionHealthControlInput"
-            >
-              <p>
-                Học sinh có tiền sửa bệnh tật/ dị ứng nào mà có thể nguy hiểm
-                tới sức khỏe không ? (Ví dụ: dị ứng nghiêm trọng với thuốc, nọc
-                của ong hoặc thức ăn, động kinh, tiểu đường, bệnh tim) /{" "}
-                <i style={{ color: "blue" }}>
-                  Does the student have any potentially life-threatening*
-                  conditions/allergies? (examples: severe allergy to a
-                  medication, bee sting or food, seizures, diabetes, heart
-                  condition)
-                </i>
-              </p>
-              <p>
-                Nếu có, vui lòng ghi rõ tình trạng và phương pháp điều trị. Nếu không, vui lòng để trống /{" "}
-                <i style={{ color: "blue" }}>
-                  If yes, please clearly explain condition and treatment. If no, please leave blank
-                </i>
-              </p>
+            <div id = {firstDomId}> 
+              <Form.Group
+                className="mb-2"
+                controlId="firstQuestionHealthControlInput"
+              >
+                <p>
+                  Học sinh có tiền sửa bệnh tật/ dị ứng nào mà có thể nguy hiểm
+                  tới sức khỏe không ? (Ví dụ: dị ứng nghiêm trọng với thuốc, nọc
+                  của ong hoặc thức ăn, động kinh, tiểu đường, bệnh tim) /{" "}
+                  <i style={{ color: "blue" }}>
+                    Does the student have any potentially life-threatening*
+                    conditions/allergies? (examples: severe allergy to a
+                    medication, bee sting or food, seizures, diabetes, heart
+                    condition)
+                  </i>
+                </p>
+                <p>
+                  Nếu có, vui lòng ghi rõ tình trạng và phương pháp điều trị. Nếu không, vui lòng để trống /{" "}
+                  <i style={{ color: "blue" }}>
+                    If yes, please clearly explain condition and treatment. If no, please leave blank
+                  </i>
+                </p>
 
-              <InputGroup className="mb-3">
-                  <Form.Control
-                    name="firstHealthQuestion"
-                    defaultValue={firstHealthQuestion}
-                  />
-              </InputGroup>
-              <hr />
-              <Form.Text>
-                <i>
-                  * Trong trường hợp học sinh có bệnh tật có thể gây nguy hiểm
-                  đến sức khỏe, GIA ĐÌNH có trách nhiệm thông báo cho Nhà trường
-                  và nhân viên y tế của Nhà trường biết trước khi con/em mình
-                  tham gia Chương trình Trại hè. /{" "}
-                  <span style={{ color: "blue" }}>
-                    {" "}
-                    If a life-threatening health condition exists, it is the
-                    responsibility of the FAMILY to notify the School and the
-                    School nurse before the student is beginning the Summer
-                    Camp.{" "}
-                  </span>{" "}
-                </i>
-              </Form.Text>
-            </Form.Group>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                      key = {firstDomId}
+                      id = {firstDomId}
+                      name={firstDomId}
+                      value={ans} onChange={(e) => setAns(e.target.value)}
+                      //ref={refFirstQuestion}
+                    />
+                </InputGroup>
+                <hr />
+                <Form.Text>
+                  <i>
+                    * Trong trường hợp học sinh có bệnh tật có thể gây nguy hiểm
+                    đến sức khỏe, GIA ĐÌNH có trách nhiệm thông báo cho Nhà trường
+                    và nhân viên y tế của Nhà trường biết trước khi con/em mình
+                    tham gia Chương trình Trại hè. /{" "}
+                    <span style={{ color: "blue" }}>
+                      {" "}
+                      If a life-threatening health condition exists, it is the
+                      responsibility of the FAMILY to notify the School and the
+                      School nurse before the student is beginning the Summer
+                      Camp.{" "}
+                    </span>{" "}
+                  </i>
+                </Form.Text>
+              </Form.Group>
+            </div>
+           
             <hr />
-            <Form.Group
-              className="mb-2"
-              controlId="secondQuestionHealthControlInput"
-            >
-              <p>
-                Học sinh có đang dùng thuốc hoặc đang điều trị mà có thể ảnh
-                hưởng đến việc tham gia các hoạt động của Chương trình Trại hè
-                không? (Ví dụ: đang dùng thuốc hen suyễn){" "}
-                <i style={{ color: "blue" }}>
-                  Is the student currently taking any medication or receiving
-                  treatment that would impact his/her participation in the
-                  Summer Camp activities? (Example: asthma inhaler)
-                </i>
-              </p>
-              <p>
-                Nếu có, vui lòng ghi rõ tình trạng và phương pháp điều trị. Nếu không, vui lòng để trống /{" "}
-                <i style={{ color: "blue" }}>If yes, please explain. If no, please leave blank</i>{" "}
-              </p>
-              <InputGroup className="mb-3">
-                  <Form.Control
-                    name="secondHealthQuestion"
-                    defaultValue={secondHealthQuestion}
-                  />
-                
-              </InputGroup>
-            </Form.Group>
+            <div id = {secondDomId}> 
+              <Form.Group
+                className="mb-2"
+                controlId="secondQuestionHealthControlInput"
+              >
+                <p>
+                  Học sinh có đang dùng thuốc hoặc đang điều trị mà có thể ảnh
+                  hưởng đến việc tham gia các hoạt động của Chương trình Trại hè
+                  không? (Ví dụ: đang dùng thuốc hen suyễn){" "}
+                  <i style={{ color: "blue" }}>
+                    Is the student currently taking any medication or receiving
+                    treatment that would impact his/her participation in the
+                    Summer Camp activities? (Example: asthma inhaler)
+                  </i>
+                </p>
+                <p>
+                  Nếu có, vui lòng ghi rõ tình trạng và phương pháp điều trị. Nếu không, vui lòng để trống /{" "}
+                  <i style={{ color: "blue" }}>If yes, please explain. If no, please leave blank</i>{" "}
+                </p>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                      id = {secondDomId}
+                      name={secondDomId}
+                      ref={refSecondQuestion}
+                    />
+                  
+                </InputGroup>
+              </Form.Group>
+            </div>
+            
           </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={()=>handleSave({refFirstQuestion,refSecondQuestion,dataModal})}>
             Save Changes
           </Button>
         </Modal.Footer>
@@ -190,10 +202,13 @@ export default function TextFields() {
           <Button onClick={handleClick}>click</Button>
           <h3 className="text-center">Students Profiles</h3>
           <DataTable
+            keyField={stuCode}
             columns={columns}
             data={studentTable}
             responsive
             highlightOnHover
+            expandableRows={true}
+        		expandableRowsComponent={ExpandedComponent}
           />
         </div>
       </div>
